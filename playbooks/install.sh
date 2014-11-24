84d9d4fc2faa5342054cd0952d5e1e177c740500
cloader89 <cameron@loaderusa.net>
2014-11-24 03:33:56 -0600

    Update install.sh

--- playbooks/install.sh
+++ playbooks/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for i in $(cat inventory); do ssh -t root@$i rm ~/.ssh/authorized_keys; ssh-keygen -t rsa; cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys; done
+for i in $(cat inventory); do ssh -t root@$i rm ~/.ssh/authorized_keys; ssh-keygen -t rsa; cat ~/.ssh/id_rsa.pub >> ssh -t root@$i ~/.ssh/authorized_keys; done
 ansible servers -m copy src=~/.ssh/authorized_keys dest=~/.ssh/authorized_keys;
 ansible-playbook ansible-openmpi.yml;
