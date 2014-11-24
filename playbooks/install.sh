1d968ca1fc7e3db298810ebef6d94d447db4e7ca
cloader89 <cameron@loaderusa.net>
2014-11-24 02:28:40 -0600

    Update install.sh

--- playbooks/install.sh
+++ playbooks/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for i in $(cat inventory); 
-    do ssh -t $i rm ~/.ssh/authorized_keys; ssh-keygen -t rsa; 
+    do ssh -t $i rm ~/.ssh/authorized_keys;  
         for j in $(cat inventory);
+            ssh-keygen -t rsa;
             do ssh-copy-id root@$j; 
         done 
 done
