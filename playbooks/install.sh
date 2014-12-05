4bcd0b2df92a35bb030bb96e1d43e43d0922ce6e
Cameron J. Loader <cameron@loaderusa.net>
2014-12-05 05:51:17 -0600

    Update install.sh

--- playbooks/install.sh
+++ playbooks/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 for i in $(cat inventory); do ssh-copy-id root@$i; done
 ansible-playbook ansible-openmpi.yml;
+for i in $(cat inventory); do cp inventory root@$i; mv inventory mpi_hosts; sed '/[host]/d'; sed '/[servers]/d'; done
+
