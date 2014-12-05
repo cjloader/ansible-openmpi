f09a0df28998ebf0b9a95f28d5882c1e4f5d7251
Cameron J. Loader <cameron@loaderusa.net>
2014-12-05 07:54:05 -0600

    Update install.sh

--- playbooks/install.sh
+++ playbooks/install.sh
@@ -3,5 +3,8 @@
 #SSH key copy
 for i in $(cat inventory); do ssh-copy-id root@$i; done
 
-#run ansible
+#run ansible for open-mpi
 ansible-playbook ansible-openmpi.yml;
+
+#run ansible for mpi_hosts
+ansible-playbook ansible-mpi_hosts.yml;
