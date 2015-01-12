3001bd602ccdeadb7cf9e937535747a4508402ae
Cameron J. Loader <cameron@loaderusa.net>
2015-01-12 16:48:47 -0600

    edited

--- playbooks/install.sh
+++ playbooks/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SSH key copy
-for i in $(cat inventory); do cat ~/.ssh/id_rsa.pub | ssh root@$i "cat >> ~/.ssh/authorized_keys"; done
+for i in $(cat inventory); do cat ~/.ssh/id_rsa.pub | ssh root@$i "rm ~/.ssh/authorized_keys; cat >> ~/.ssh/authorized_keys"; done
 
 #run ansible for open-mpi
 ansible-playbook ansible-openmpi.yml;
