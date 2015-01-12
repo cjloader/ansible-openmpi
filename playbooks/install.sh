1f7d4d5d23ff1a2a06d6a79c88ca48851af04ebb
Cameron J. Loader <cameron@loaderusa.net>
2015-01-12 16:02:49 -0600

    Updated the program to be more efficient

--- playbooks/install.sh
+++ playbooks/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SSH key copy
-for i in $(cat inventory); do ssh-copy-id root@$i; done
+for i in $(cat inventory); do cat ~/.ssh/id_rsa.pub | ssh root@$i "cat >> ~/.ssh/authorized_keys"; done
 
 #run ansible for open-mpi
 ansible-playbook ansible-openmpi.yml;
