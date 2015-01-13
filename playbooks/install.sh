60ef189e94973ac07748c9fba467bf921a6d1371
Cameron J. Loader <cameron@loaderusa.net>
2015-01-13 15:06:52 -0600

    Update install.sh

--- playbooks/install.sh
+++ playbooks/install.sh
@@ -2,10 +2,10 @@
 
 #SSH key copy
 for i in $(cat inventory); do cat ~/.ssh/id_rsa.pub | ssh root@$i "rm ~/.ssh/authorized_keys; cat >> ~/.ssh/authorized_keys"; done
-ansible-playbook ansible-ssh-keygen.yml
-
 #run ansible for open-mpi
 ansible-playbook ansible-openmpi.yml;
+
+ansible-playbook ansible-ssh-keygen.yml;
 
 #run ansible for mpi_hosts
 ansible-playbook ansible-mpi_hosts.yml;
