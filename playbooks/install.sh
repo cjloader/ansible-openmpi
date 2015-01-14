b952f59fcc974b27913cb69a8006da2bf99e61a1
Cameron J. Loader <cameron@loaderusa.net>
2015-01-13 23:10:06 -0600

    Update install.sh

--- playbooks/install.sh
+++ playbooks/install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 
-#install build essential
-for i in $(cat inventory); do ssh root@$i 'sudo apt-get install build-essential';  cat ~/.ssh/id_rsa.pub | ssh root@$i "rm ~/.ssh/authorized_keys; cat >> ~/.ssh/authorized_keys"; done
 #SSH key copy 
 ansible-playbook ansible-ssh-keygen.yml;
 #run ansible for open-mpi
