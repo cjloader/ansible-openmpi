960648497e0ef4497aecb1a83fe21daffec9bf3f
Cameron J. Loader <cameron@loaderusa.net>
2015-01-15 13:53:46 -0600

    Update install.sh

--- playbooks/install.sh
+++ playbooks/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 #SSH key copy 
-ansible-playbook ansible-ssh-keygen.yml --ask-pass;
-#run ansible for open-mpi
-ansible-playbook ansible-openmpi.yml --ask-pass;
-
+ansible-playbook ansible-ssh-keygen.yml ansible-openmpi.yml ansible-mpi_hosts.yml --ask-pass;
 #run ansible for mpi_hosts
-ansible-playbook ansible-mpi_hosts.yml --ask-pass;
+#ansible-playbook ansible-mpi_hosts.yml --ask-pass;
